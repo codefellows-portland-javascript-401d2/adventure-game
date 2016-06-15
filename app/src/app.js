2f47a28557c32140bbb2013bcd22a9a5ea814ecd
DonChatelain <djchatelain@gmail.com>
2016-06-15 16:51:55 -0700

    adds settings file

--- app/src/app.js
+++ app/src/app.js
@@ -1,8 +1,9 @@
 const angular = require('angular');
 const material = require('angular-material');
-var player = require('./lib/player');
-var Monster = require('./lib/monster');
-var gameState = require('./lib/game-state');
+const player = require('./lib/player');
+const Monster = require('./lib/monster');
+const gameState = require('./lib/game-state');
+const settings = require('./lib/settings');
 
 require('./css/main.css');
 
@@ -28,10 +29,34 @@ app.controller('main-Ctrl', function($scope) {
 
   $scope.player = player;
 
+  $scope.inputA = 'Melee';
+  $scope.inputB = 'Ranged';
+  $scope.inputC = 'Evade';
+  $scope.inputD = 'Punch Self';
+
+  $scope.input = function(button) {  // Needs to be dynamic according to State
+    if (button === 'A') {
+      $scope.decision = 'You attack with melee weapon';
+      player.stamina -= settings.meleeCost;
+    }
+    else if (button === 'B') {
+      $scope.decision = 'You attack with ranged weapon';
+      player.stamina -= settings.rangedCost;
+    }
+    else if (button === 'C') {
+      $scope.decision = 'You try to evade';
+
+    }
+    else if (button === 'D') {
+      $scope.decision = 'Stop hitting yourself';
+      player.health -= settings.rangedDamage;
+    }
+  };
+
   $scope.decision = null;
   $scope.action = function(text = $scope.decision){
     if (text){
-      if (text === 'Evade'){
+      if (text === 'You try to evade'){
         return text + '.  The monster attacks and misses!';
       }else if (text === 'Stop hitting yourself'){
         return text + '. The monster giggles.';
---
+++ app/src/lib/settings.js
@@ -0,0 +1,6 @@
+module.exports = {
+  meleeDamage: 8,
+  rangedDamage: 4,
+  meleeCost: 10,
+  rangedCost: 5
+};
