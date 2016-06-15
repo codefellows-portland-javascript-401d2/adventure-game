27e6b0515229cee70f65c5a984cb2944e1cb7de5
Yvonne Hayes <queyvonne@gmail.com>
2016-06-15 15:07:18 -0700

    added evade functionality to the battle logic example

--- app/src/lib/battle.js
+++ app/src/lib/battle.js
@@ -8,11 +8,12 @@ var player = {
 
 var mob = {
   name: 'FancySnake',
-  hp: 50
+  hp: 50,
+  attack: 5
 };
 
 battle.getsHit = function(){
-  var newHp = player.hp - 5;
+  var newHp = player.hp - mob.attack;
   console.log('HP: ' + newHp);
   player.hp = newHp;
 
@@ -48,21 +49,42 @@ battle.attacksMelee = function(){
   }
 };
 
-battle.attackAndStaminaRegen = function(){
+function randomNumber(min,max) {
+  return Math.floor(Math.random()*(max-min+1)+min);
+}
 
-  attacksMelee();
-  var newStamina = player.stamina + 3;
-  player.stamina = newStamina;
+battle.evadeAttack = function(){
+
+  var random = randomNumber(1,6);
+
+  if (random < 3) {
+
+    console.log('Your evade was successfull!');
+    var newStamina = player.stamina + 3;
+    console.log('You have gained ' + newStamina + ' Stamina!');
+    player.stamina = newStamina;
+
+  } else if (random < 5) {
+
+    console.log('Your evade failed!');
+    var newHp = player.hp - mob.attack;
+    console.log('HP: ' + newHp);
+    player.hp = newHp;
+
+  } else {
+
+    console.log('Your evade did not quite work out. You still got hit!');
+
+    var newHp = player.hp - (mob.attack * 0.5);
+    console.log('HP: ' + newHp);
+    player.hp = newHp;
+
+
+    console.log('You have gained ' + newStamina + ' Stamina!');
+    player.stamina += 1;
+  }
 };
 
-attackAndStaminaRegen();
-attackAndStaminaRegen();
-attackAndStaminaRegen();
-attackAndStaminaRegen();
-attackAndStaminaRegen();
-attackAndStaminaRegen();
-attackAndStaminaRegen();
-attackAndStaminaRegen();
 
 
 module.exports = battle;
