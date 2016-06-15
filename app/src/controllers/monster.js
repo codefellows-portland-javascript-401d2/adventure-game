e2446bb6647757fd68c6f22ee4a75f743fc67fb6
DonChatelain <djchatelain@gmail.com>
2016-06-14 17:24:05 -0700

    monster controller

---
+++ app/src/controllers/monster.js
@@ -0,0 +1,18 @@
+
+monsterController.$inject = ['$scope'];
+
+function monsterController(monster) {
+  // monster.dancingBears = new Monster('Dancing Bears', 30, 15, 0.5, 'r');
+  // monster.funnySnakes = new Monster('Funny Snakes', 10, 5, 0.9, 'm');
+}
+
+// var Monster = function(name, health, damage, accuracy, weakness) {
+//   this.name = name;
+//   this.health = health;
+//   this.damage = damage;
+//   this.accuracy = accuracy;
+//   this.weakness = weakness;
+// };
+
+
+module.exports = monsterController;
