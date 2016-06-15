04d29c65a820728d60fb14804e0d54c9e908ec8d
DonChatelain <djchatelain@gmail.com>
2016-06-15 15:34:41 -0700

    modularizes monster and player data- introduces game state

---
+++ app/src/lib/game-state.js
@@ -0,0 +1,20 @@
+const gameState = {};
+
+gameState.levels = [
+  'Intro',
+  'Sewers',
+  'Battle in the Sewers',
+  'Dungeon',
+  'Battle in the Dungeon',
+  'Antechamber',
+  'Battle in the Antechamber',
+  'Oratory',
+  'Battle in the Oratory',
+  'Base of the High Tower',
+  'Final Battle'
+];
+
+
+
+
+module.exports = gameState;
