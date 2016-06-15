fbea4bd489d21ebca38c7df1848d648665023d51
DonChatelain <djchatelain@gmail.com>
2016-06-14 19:35:08 -0700

    adds angular material, and health bars! who hoo!

--- app/src/app.js
+++ app/src/app.js
@@ -1,9 +1,17 @@
 const angular = require('angular');
-const playerCtrl = require('./controllers/player');
-const monsterCtrl = require('./controllers/monster');
+const material = require('angular-material');
+
 require('./css/main.css');
 
-const app = angular.module('app', []);
+const app = angular.module('app', [material]);
+
+app.config(function($mdThemingProvider) {
+  $mdThemingProvider.theme('statusBars')
+    .primaryPalette('red')
+    .accentPalette('green')
+    .warnPalette('blue')
+    .backgroundPalette('grey');
+});
 
 app.controller('main-Ctrl', function($scope) {
   $scope.action = 'Make a Move!';
@@ -13,10 +21,12 @@ app.controller('action-Ctrl', function($scope) {
   
   var Monster = function(name, health, damage, accuracy, weakness) {
     this.name = name;
-    this.health = health;
+    this.maxHealth = health;
     this.damage = damage;
     this.accuracy = accuracy;
     this.weakness = weakness;
+    this.health = this.maxHealth;
+    // this.currentHealth = (this.health * 100) / this.maxHealth; 
   };
 
   var player = {
@@ -28,6 +38,11 @@ app.controller('action-Ctrl', function($scope) {
   $scope.player = player;
   $scope.dancingBears = new Monster('Dancing Bears', 30, 15, 0.5, 'r');
   $scope.funnySnakes = new Monster('Funny Snakes', 10, 5, 0.9, 'm');
+
+  $scope.$watch('dancingBears.health', function(newValue) {
+    $scope.dancingBears.currentHealth = (newValue * 100) / $scope.dancingBears.maxHealth;
+  });
+
 });
 
 // app.controller('player-Ctrl', playerCtrl);
